6901eb2ce1ebf675c2f2693f8b691fff2e0f1f3c
suhyeon21 <suhyeon.kan@gmail.com>
2022-01-22 22:57:01 +0900

    feat: 테이블 태그 사용해서 게시판 리스트처럼 출력

--- src/App.js
+++ src/App.js
@@ -20,11 +20,39 @@ class App extends Component {
   };
   render() {
     const { boards } = this.state; //const boards = this.state.boards
-    const list = boards.map(function (row) {
-      return row.brdno + row.brdwriter;
-    });
 
-    return <div>{list}</div>;
+    return (
+      <div>
+        <table border="1">
+          <tbody>
+            <tr align="center">
+              <td width="50">No.</td>
+              <td width="300">Title</td>
+              <td width="100">Name</td>
+              <td width="100">Date</td>
+            </tr>
+            {boards.map((row) => (
+              <BoardItem key={row.brdno} row={row} /> //BoardItem 이라고 앞에 왜 적었지? Boarditem 컴포넌트 이용하려고?
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class BoardItem extends React.Component {
+  render() {
+    const r = this.props.row;
+    console.log(r);
+    return (
+      <tr>
+        <td>{this.props.row.brdno}</td>
+        <td>{this.props.row.brdtitle}</td>
+        <td>{this.props.row.brdwriter}</td>
+        <td>{this.props.row.brddate.toLocaleDateString("ko-KR")}</td>
+      </tr>
+    );
   }
 }
 
